5881cd31ec20cd9e0bdc5dde5ba23efdb74df7d2
unknown <95885719+fotmjay@users.noreply.github.com>
2023-09-08 14:31:52 -0400

    fixed username length validation to be 6 like intended

--- helpers/validateFields.js
+++ helpers/validateFields.js
@@ -4,7 +4,7 @@ function validateUser(data) {
   const validationErrors = [];
   data.email = validator.trim(data.email).toLowerCase();
   data.username = validator.trim(data.username).toLowerCase();
-  if (data.username.length < 5) validationErrors.push({ message: "Username needs to be at least 6 characters." });
+  if (data.username.length < 6) validationErrors.push({ message: "Username needs to be at least 6 characters." });
   if (data.password.length < 8) validationErrors.push({ message: "Password needs to be at least 8 characters." });
   if (!validator.isEmail(data.email)) validationErrors.push({ message: "Please enter a valid email address." });
   if (data.password !== data.confirmPass || validator.isEmpty(data.password))
